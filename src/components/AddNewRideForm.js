a3603187c57098b0faf45282e7041784ecf00a72
pannaAdrianna <250319@student.pwr.edu.pl>
2021-12-14 13:48:16 +0100

    update AddNewForm.js ui

--- src/components/AddNewRideForm.js
+++ src/components/AddNewRideForm.js
@@ -32,6 +32,9 @@ const RideForm = () => {
 
     const onFinish = (values) => {
         console.log(values);
+
+        // tu dodaj wysyłanie gdzieś -> chyba na serwer
+        form.resetFields();
     };
 
     const onReset = () => {
